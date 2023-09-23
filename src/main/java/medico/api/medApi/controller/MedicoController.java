b2fa663b7fc47d185f590d38380167241cd574d0
vinipolitta <vinicius.politta1@gmail.com>
2023-09-23 07:34:33 -0300

    Created pagination

--- src/main/java/medico/api/medApi/controller/MedicoController.java
+++ src/main/java/medico/api/medApi/controller/MedicoController.java
@@ -3,10 +3,16 @@ package medico.api.medApi.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import medico.api.medApi.medico.DadosCadastroMedico;
+import medico.api.medApi.medico.DadosListagemMedico;
 import medico.api.medApi.medico.Medico;
 import medico.api.medApi.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,5 +25,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
---
+++ src/main/java/medico/api/medApi/medico/DadosListagemMedico.java
@@ -0,0 +1,20 @@
+package medico.api.medApi.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import medico.api.medApi.endereco.DadosEndereco;
+
+public record DadosListagemMedico(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade
+     ) {
+
+     public DadosListagemMedico(Medico medico){
+          this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+     }
+}
